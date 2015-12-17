a05f6e9b8212d9fbbee95bd8d346ed7209e6f78d
Luke Maciak <luke@maciak.net>
2015-12-17 14:38:32 -0500

    Character counter

--- public/validate.js
+++ public/validate.js
@@ -39,3 +39,24 @@ $("#spoilerform").submit(function() {
 
     return source && spoiler && tweet;
 });
+
+
+var max_count = 117 //maximum count 140 char minus 22 char for url shortener
+
+$('#tweet').focus(function(){
+  $(document).keyup(function(){
+      
+      var char_count = $('#tweet').val().length;
+      var remaining_char = max_count - char_count;
+      
+      if (char_count < max_count) {
+        $('#count').removeClass('red_text')
+        .addClass('green_text')
+        .text(' remaining '+remaining_char);
+      } else {
+        $('#count').removeClass('green_text')
+        .addClass('red_text')
+        .text(remaining_char);
+      }
+  });
+});
